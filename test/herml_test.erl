6818755f9d3f5a27d8aca47f921d66d7afcace1a
Tim McGilchrist <timmcgil@gmail.com>
2013-02-06 08:06:49 +1100

    Add test for both file and binary.

--- test/herml_test.erl
+++ test/herml_test.erl
@@ -4,5 +4,8 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-compile_file_test() ->
+compile_binary_test() ->
     {ok, compile_test} = herml:compile(<<"%html">>, compile_test).
+
+compile_file_test() ->
+    {ok, compile_test} = herml:compile("./examples/basic_html.herml", compile_test).
